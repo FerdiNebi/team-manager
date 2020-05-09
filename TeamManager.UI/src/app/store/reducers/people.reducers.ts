95372556981d23bcca01ddc59e86970d69deac3d
Ferdi Nebiev <ferdi.nebiev@gmail.com>
2020-05-10 01:55:50 +0300

    Fix empty screen buttons

--- TeamManager.UI/src/app/store/reducers/people.reducers.ts
+++ TeamManager.UI/src/app/store/reducers/people.reducers.ts
@@ -1,18 +1,26 @@
 import * as peopleActions from '../actions/people.actions';
 import { Person } from 'src/app/people/person';
 
-const initialState: Person[] = [];
+const initialState: Person[] = null;
 
-export function peopleReducer(state = initialState, action: peopleActions.PeopleActions ): Person[] {
+export function peopleReducer(state = initialState, action: peopleActions.PeopleActions): Person[] {
     switch (action.type) {
         case peopleActions.PeopleActionTypes.GetPeopleSuccess: {
             return action.payload;
         }
         case peopleActions.PeopleActionTypes.AddPersonSuccess: {
-            return [action.payload, ...state];
+            if (state) {
+                return [action.payload, ...state];
+            }
+            
+            return [action.payload];
         }
         case peopleActions.PeopleActionTypes.DeletePersonSuccess: {
-            return state.filter(p => p.id !== action.payload);
+            if (state) {
+                return state.filter(p => p.id !== action.payload);
+            }
+
+            return state;
         }
         default: {
             return state;
